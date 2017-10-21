2a615c47e7aba036413e1b9de9bf74066a0f2b19
korgoth <barbaria.korgoth@gmail.com>
2017-10-21 19:57:37 +0200

    Add sub interface

--- src/com/beauce/tutorial/winterbe/java8/defaultmethod/DefaultMethodTest.java
+++ src/com/beauce/tutorial/winterbe/java8/defaultmethod/DefaultMethodTest.java
@@ -7,6 +7,13 @@ interface A{
 	}
 }
 
+interface B extends A{
+	void doOtherThings();
+	default int diff(int a, int b){
+		return a - b;
+	}
+}
+
 public class DefaultMethodTest {
 	public static void main(String[] args) {
 		System.out.println("default method test");
@@ -16,6 +23,18 @@ public class DefaultMethodTest {
 				System.out.println("do something");
 			}
 		};
+		
+		B b = new B(){
+			@Override
+			public void doSomething() {
+				System.out.println("do something");
+			}
+			@Override
+			public void doOtherThings() {
+				System.out.println("do other thing");
+			}
+		};
 		System.out.println("Sum: " + a.sum(1, 2));
+		System.out.println("Diff:" + b.sum(1, 2) + "/" + b.diff(5, 3));
 	}
 }
