07510c7015d47c1b2c4c883d7c6f19f9db129f96
korgoth <barbaria.korgoth@gmail.com>
2017-10-21 19:10:02 +0200

    First version project

--- src/com/beauce/tutorial/winterbe/java8/defaultmethod/DefaultMethodTest.java
+++ src/com/beauce/tutorial/winterbe/java8/defaultmethod/DefaultMethodTest.java
@@ -1,5 +1,21 @@
 package com.beauce.tutorial.winterbe.java8.defaultmethod;
 
-public class DefaultMethodTest {
+interface A{
+	void doSomething();
+	default int sum(int a, int b){
+		return a + b;
+	}
+}
 
+public class DefaultMethodTest {
+	public static void main(String[] args) {
+		System.out.println("default method test");
+		A a = new A(){
+			@Override
+			public void doSomething() {
+				System.out.println("do something");
+			}
+		};
+		System.out.println("Sum: " + a.sum(1, 2));
+	}
 }
